1d505988ee8549f8b4da9dbad798bc0f104307ed
Jean Uchôa <jean.uchoa@gmail.com>
2021-07-22 20:11:08 -0300

    extract config to an YAML file

--- quickman.py
+++ quickman.py
@@ -1,31 +1,37 @@
 import requests
 import os
+import yaml
 from jsonpath import JSONPath
 from dotenv import load_dotenv
+from collections import ChainMap
 
-base_path = [
-    "$",
-    "'dot11.device'",
-    "'dot11.device.last_beaconed_ssid_record'",
-]
+def config():
+    with open('config.yml') as content:
+        return yaml.safe_load(content)
 
-attributes_paths =  {
-    "ssid": "'dot11.advertisedssid.ssid'",
-    "channel": "'dot11.advertisedssid.channel'",
-}
+def wrap_segments(path):
+    return map(lambda segment: "'{s}'".format(s=segment), path)
 
-def jsonpath(entry):
-    return ".".join(base_path + [entry])
+def complete_path(path):
+    return '.'.join(["$"] + list(wrap_segments(path)))
 
-full_paths = dict(zip(
-    attributes_paths.keys(),
-    map(lambda entry: jsonpath(entry), attributes_paths.values())
-))
+def entries_full_paths(entry):
+    base = entry['base']
+    attrs = entry['fields']
+    return dict(map(
+        lambda attr: [attr, complete_path(base + [attrs[attr]])],
+        attrs.keys()
+    ))
+
+def all_complete_paths():
+    c = config["kismet"]
+    return ChainMap(*map(lambda group: entries_full_paths(c[group]), c.keys()))
 
 def extract_data(data):
-    return dict(zip(
-        full_paths.keys(),
-        map(lambda q: "".join(JSONPath(q).parse(data)), full_paths.values())
+    return dict(map(
+        lambda attr, query: [attr, "".join(JSONPath(query).parse(data))],
+        all_complete_paths().keys(),
+        all_complete_paths().values()
     ))
 
 def url():
@@ -35,29 +41,25 @@ def url():
     )
 
 def kismet_data():
-    response = ""
+    response = {}
     try:
         response = requests.get(url()).json()
     except Exception as e:
-        response = {'error': {'message': 'kismet down', 'response': e}}
+        response = {'error': {'kismet': 'offline', 'response': e}}
     return response
 
 def network_data(kismet_response):
     return filter(valid_data, map(extract_data, kismet_response))
 
-
-def network_attributes():
-    return ['ssid', 'channel']
-
 def valid_data(network_data):
     """verify existence of all fields with non-empty value"""
-    return all(map(lambda attr: network_data[attr], network_attributes()))
+    return all(map(lambda attr: network_data[attr], all_complete_paths().keys()))
 
+config = config()
 def main():
     # use local .env to override ENVVARS
     load_dotenv()
 
-    kismet_response = kismet_data()
+    kismet_response = kismet_data() or {'error': {'kismet': 'no data'}}
 
     return kismet_response['error'] or network_data(kismet_response)
-
