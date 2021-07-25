62a6180690a2bc22f6af46ba06be7c96769291f3
Jean Uchoa <jean.uchoa@gmail.com>
2021-07-25 13:36:38 -0300

    fix lint warnings and change return of main

--- quickman.py
+++ quickman.py
@@ -1,74 +1,103 @@
+"""" kismet data extract """
+
 import json
 import os
+from collections import ChainMap
+
 import requests
 import yaml
-from collections import ChainMap
 from dotenv import load_dotenv
 from jsonpath import JSONPath
 
+
 def dump_kistmet_data():
-    with open('kismet_response.json', 'w') as f:
-      json.dump(kismet_data(), f, ensure_ascii=False, sort_keys = True, indent = 2)
+    """ dump kismet data in formated file """
+    with open('kismet_response.json', 'w') as file:
+        json.dump(kismet_data(),
+                  file,
+                  ensure_ascii=False,
+                  sort_keys=True,
+                  indent=2)
+
 
 def config():
+    """ config in json """
     with open('config.yml') as content:
         return yaml.safe_load(content)
 
+
 def wrap_segments(path):
+    """ wrap all jsonpath segments with single quotes """
     return map(lambda segment: "'{s}'".format(s=segment), path)
 
+
 def jsonpath(path):
+    """ generate jsonpath string """
     return '.'.join(["$"] + list(wrap_segments(path)))
 
-def full_paths(entry):
+
+def realpaths(entry):
+    """ concatenate the base segment with the attr segment """
     base = entry['base']
     paths = entry['fields']
-    complete_path = lambda attr_name: base + [paths[attr_name]]
-    return dict(map(
-        lambda attr: [attr, jsonpath(complete_path(attr))],
-        paths.keys()
-    ))
+    path = lambda attr_name: base + [paths[attr_name]]
+    return dict(map(lambda attr: [attr, jsonpath(path(attr))], paths.keys()))
+
 
 def jsonpaths():
-    c = config["kismet"]
-    return dict(ChainMap(*map(lambda group: full_paths(c[group]), c.keys())))
+    """ all generate jsonpaths contained in config file """
+    cfg = config["kismet"]
+    return dict(
+        ChainMap(*map(lambda group: realpaths(cfg[group]), cfg.keys())))
 
-def extract_data(data):
-    return dict(map(
-        lambda attr, query: [attr, parse_data(query, data)],
-        jsonpaths().keys(),
-        jsonpaths().values()
-    ))
 
-def parse_data(query, json):
-    result = JSONPath(query).parse(json)
+def extract_data(data_node):
+    """ parse data from each node in data """
+    return dict(
+        map(lambda attr, query: [attr, parse_data(query, data_node)],
+            jsonpaths().keys(),
+            jsonpaths().values()))
+
+
+def parse_data(query, kismet_json):
+    """ return jsonpath return value """
+    result = JSONPath(query).parse(kismet_json)
     return "" if not result else result.pop()
 
 
 def url():
+    """ generate kismet url using ENVVARS to fetch credentials """
     return "http://{user}:{password}@127.0.0.1:2501/devices/views/all/devices.json".format(
-        user = os.environ["KISMET_USER"],
-        password = os.environ["KISMET_PASSWORD"]
-    )
+        user=os.environ["KISMET_USER"], password=os.environ["KISMET_PASSWORD"])
 
-def kismet_data():
+
+def kismet_response():
+    """ fetch data from kismet endpoint in json format """
     response = {}
     try:
         response = requests.get(url()).json()
-    except Exception as e:
-        response = {'error': {'kismet': 'offline', 'response': e}}
+        response = {'ok': response} if response else {'error': 'no_data'}
+    except requests.exceptions.RequestException as exception:
+        response = {'error': {'kismet': 'offline', 'response': exception}}
     return response
 
-def network_data(kismet_response):
-    return filter(valid_data, map(extract_data, kismet_response))
+
+def data(response):
+    """ the networj data with all attributes filleds """
+    return filter(valid_data, map(extract_data, response))
+
 
 def valid_data(network_data):
     """verify existence of all fields with non-empty value"""
     return all(map(lambda attr: network_data[attr], jsonpaths().keys()))
 
+
 config = config()
+
+
 def main():
+    """ main function """
     # use local .env to override ENVVARS
     load_dotenv()
-    resp = kismet_data() or {'error': {'kismet': 'no data'}}
-    return list(network_data(resp)) if isinstance(resp, list) else resp['error']
+    resp = kismet_response()
+    return resp['error'] or list(data(resp))
